ca0f948996c9c1fdb7103527637ca1ab7b32cce4
martalexaa <63308375+martalexaa@users.noreply.github.com>
2023-04-26 10:30:41 +0200

    Updated case study

--- pages/myflix_case_study.js
+++ pages/myflix_case_study.js
@@ -3,9 +3,11 @@ import Link from 'next/link'
 import Image from 'next/image';
 import mainview from "../public/movie_app_screenshots/mainview.png";
 import movies from "../public/movie_app_screenshots/movies.png";
-import updatedata from "../public/movie_app_screenshots/updatedata.png"
-import movieview from "../public/movie_app_screenshots/movieview.png"
-import card from "../public/movie_app_screenshots/card.png"
+import updatedata from "../public/movie_app_screenshots/updatedata.png";
+import movieview from "../public/movie_app_screenshots/movieview.png";
+import models from "../public/movie_app_screenshots/models.png";
+import backend from "../public/movie_app_screenshots/backend.png";
+import card from "../public/movie_app_screenshots/card.png";
 
 const MyflixCase = () => (
     <>
@@ -23,14 +25,17 @@ const MyflixCase = () => (
                 <br />
                 Movie App
             </h1>
-            <div className="text-gray-600 px-2 lg:px-20">
-                <Image
-                    src={movies}
-                    width={"100%"}
-                    height={"100%"}
-                    alt="screenshot of the movie app"
-                    className="drop-shadow-md" />
-                <h2 className="text-xl md:text-2xl pb-2 pt-10 font-semibold">Overview:
+            <div>
+                <figure className="text-gray-600 px-5 lg:px-20">
+                    <Image
+                        src={movies}
+                        width={"100%"}
+                        height={"100%"}
+                        alt="screenshot of the movie app"
+                        className="drop-shadow-md" />
+                    <figcaption className="text-center mt-2">Figure 1. Users can add movies to their favorites by clicking the heart icons</figcaption>
+                </figure>
+                <h2 className="text-xl md:text-2xl pb-2 pt-10 font-semibold lg:pt-20">Overview:
                 </h2>
                 <p className="pb-10 lg:text-lg">The Movie App is a MERN web application that allows movie enthusiasts to access information about top-rated movies of all time.
                     Users can sign up, log in, update their data, delete their accounts, and choose their favorite movies which will be displayed in the profile view.
@@ -64,13 +69,14 @@ const MyflixCase = () => (
                         </ul>
                     </div>
 
-                    <div className="flex-3/4">
+                    <figure className="flex-3/4">
                         <Image src={mainview}
                             className="m-2 drop-shadow-md"
                             width={"100%"}
                             height={"100%"}
                             alt="screenshot of the movie app" />
-                    </div>
+                        <figcaption className="text-center mt-2">Figure 2. The main view</figcaption>
+                    </figure>
                 </div>
 
                 <h2 className="text-xl md:text-2xl pb-2 pt-5 font-semibold">Tech Stack:</h2>
@@ -91,14 +97,15 @@ const MyflixCase = () => (
                 </h2>
                 <p className="pb-10 lg:text-lg">First, I needed to get familiar with the project requirements outlined by Careerfoundry, including the user stories, features, and technical requirements. I started with the server side by setting up the database.</p>
                 <div className="lg:flex">
-                    <div className="flex-1 p-2">
+                    <figure className="flex-1 p-2">
                         <Image
                             src={card}
                             width={"100%"}
                             height={"100%"}
                             alt="screenshot of the movie app"
                             className="drop-shadow-md" />
-                    </div>
+                        <figcaption className="text-center mt-2">Figure 3. A single movie card</figcaption>
+                    </figure>
                     <div className="flex-1">
                         <h2 className="text-lg md:text-xl pb-2">Database
                         </h2>
@@ -121,9 +128,29 @@ const MyflixCase = () => (
                 </div>
                 <h2 className="text-lg md:text-xl pb-2 lg:pt-10">Deployment:
                 </h2>
-                <p className="pb-2 lg:text-lg">The Movie App was deployed on Netlify, a popular hosting service for web applications, making it accessible online for users.
+                <p className="pb-5 lg:pb-20 lg:text-lg">The Movie App was deployed on Netlify, a popular hosting service for web applications, making it accessible online for users.
                 </p>
-                <h2 className="text-xl md:text-2xl pb-8 pt-5 lg:pt-10 font-semibold">Challenges:
+
+                <div div className="lg:flex">
+                    <figure className="flex-2/4">
+                        <Image src={models}
+                            className="p-2 drop-shadow-md"
+                            width={"100%"}
+                            height={"100%"}
+                            alt="screenshot of the movie app" />
+                        <figcaption className="text-center mt-2">Figure 4. Movie and user models</figcaption>
+                    </figure>
+                    <figure className="flex-2/4">
+                        <Image src={backend}
+                            className="p-2 drop-shadow-md"
+                            width={"100%"}
+                            height={"100%"}
+                            alt="screenshot of the movie app" />
+                        <figcaption className="text-center mt-2">Figure 5. Code snippet from the backend</figcaption>
+                    </figure>
+                </div>
+
+                <h2 className="text-xl md:text-2xl pb-8 pt-5 lg:pt-20 font-semibold">Challenges:
                 </h2>
                 <p className="pb-2 lg:text-lg">The main challenge was gaining a solid understanding of how React works and managing the interaction between components. The component-based architecture requires a different approach compared to traditional HTML-based web development. I struggled with structuring the components correctly, passing data between them, and managing the state effectively.
                     Careerfoundry provided step-by-step instructions for creating the first components, and I felt like I understood React well. However, when I had to build the profile view from scratch, I suddenly felt lost.
@@ -131,30 +158,32 @@ const MyflixCase = () => (
                 <p className="pb-2 lg:text-lg">
                     I had to push myself to step out of my comfort zone and dive deep into understanding React concepts and syntax. With determination and the support of my mentor and, I gradually gained confidence in my abilities. I learned to break down complex problems into smaller, manageable tasks and approached them systematically.
                 </p>
-                <p className="pb-10 lg:text-lg">
+                <p className="pb-5 lg:pb-20 lg:text-lg">
                     As a result, I successfully implemented all the views and completed the achievement.
                 </p>
 
                 <div div className="lg:flex">
-                    <div className="flex-2/4">
+                    <figure className="flex-2/4">
                         <Image src={movieview}
                             className="p-2 drop-shadow-md"
                             width={"100%"}
                             height={"100%"}
                             alt="screenshot of the movie app" />
-                    </div>
-                    <div className="flex-2/4">
+                        <figcaption className="text-center mt-2">Figure 6. Users can read about a movie in the movie view</figcaption>
+                    </figure>
+                    <figure className="flex-2/4">
                         <Image src={updatedata}
                             className="p-2 drop-shadow-md"
                             width={"100%"}
                             height={"100%"}
                             alt="screenshot of the movie app" />
-                    </div>
+                        <figcaption className="text-center mt-2">Figure 7. Update data in the profile view</figcaption>
+                    </figure>
                 </div>
 
-                <h2 className="text-xl md:text-2xl pb-8 pt-5 font-semibold">Conclusion
+                <h2 className="text-xl md:text-2xl pb-8 pt-5 lg:pt-20 font-semibold">Conclusion
                 </h2>
-                <p className="pb-10 lg:text-lg">Looking back, I am proud of how I overcame the challenges so that I was able to deliver my first MERN app. While thinking in React was not easy initially, diving into it and gaining a better understanding made it a fun and rewarding experience.
+                <p className="pb-10 lg:text-lg lg:pb-20">Looking back, I am proud of how I overcame the challenges so that I was able to deliver my first MERN app. While thinking in React was not easy initially, diving into it and gaining a better understanding made it a fun and rewarding experience.
                 </p>
             </div>
 
